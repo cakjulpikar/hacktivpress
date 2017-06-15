883ec617f5dd94c1838813bdcaec0e438998ac98
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-06-15 13:29:23 +0700

    30 minute 1

---
+++ server/app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
+var cors = require('cors')
+
+mongoose.connect('mongodb://localhost/hacktivpress',function (err) {
+  if (err) {
+    console.log('Database Error', err);
+  } else {
+    console.log("MongoDB Connected");
+  }
+})
+
+var articles = require('./routes/articles');
+var users = require('./routes/users');
+
+var app = express();
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api/articles', articles);
+app.use('/api/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error');
+});
+
+module.exports = app;
---
+++ server/routes/users.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/signin')
+router.post('signup')
+
+module.exports = router;
